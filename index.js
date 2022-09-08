1c4760113b81ec9de380cbc153586b0193c8b446
DimashIITU <dinmuhamedserik4@gmail.com>
2022-09-08 18:05:17 +0600

    27

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const upload = multer({ storage });
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://blog-front-phi.vercel.app/',
+    origin: 'https://blog-front-phi.vercel.app',
     methods: ['GET', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-Width', 'Authorization', 'Accept'],
     exposedHeaders: ['Content-Range', 'X-Content-Range'],
@@ -57,18 +57,7 @@ app.use(
     optionsSuccessStatus: 200,
   }),
 );
-app.options(
-  '*',
-  cors({
-    origin: 'https://blog-front-phi.vercel.app/',
-    methods: ['GET', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-Width', 'Authorization', 'Accept'],
-    exposedHeaders: ['Content-Range', 'X-Content-Range'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 200,
-  }),
-);
+app.options('*', cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
