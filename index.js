0cd3cc4e20c2c318d9f8158066234116713aa08b
DimashIITU <dinmuhamedserik4@gmail.com>
2022-09-08 15:26:28 +0600

    onemore

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const upload = multer({ storage });
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://myblogname.herokuapp.com',
+    origin: 'https://blog-front-phi.vercel.app/',
     methods: ['GET', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-Width', 'Authorization', 'Accept'],
     exposedHeaders: ['Content-Range', 'X-Content-Range'],
