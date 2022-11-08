5985b73831e096ae911b92ad4a2e49dbb1bd1a08
DimashIITU <dinmuhamedserik4@gmail.com>
2022-11-08 12:06:48 +0600

    tgbrew

--- cors/index.js
+++ cors/index.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://blog-front-phi.vercel.app',
+  origin: '*',
   methods: ['GET', 'HEAD', 'POST', 'PATCH', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-Width', 'Authorization', 'Accept'],
   optionsSuccessStatus: 200,
